596333b17253032033b3c9581bbc73f3a56d8131
koto1011 <koto1011@hs-karlsruhe.de>
2011-12-13 21:49:48 +0100

    Aufruf für createSprintDialogActivity eingefügt (noch provisorisch)

--- FeatureRoadmap/gen/com/wpfandroid/presentation/R.java
+++ FeatureRoadmap/gen/com/wpfandroid/presentation/R.java
@@ -17,27 +17,30 @@ public final class R {
         public static final int tooltip_unten=0x7f020003;
     }
     public static final class id {
-        public static final int Pawn=0x7f050001;
-        public static final int beschriftung=0x7f05000c;
-        public static final int buttons=0x7f050003;
-        public static final int item=0x7f050000;
-        public static final int linearLayout1=0x7f050005;
-        public static final int loadRoadmap=0x7f050007;
-        public static final int newRoadmap=0x7f050006;
-        public static final int newRoadmapName=0x7f050004;
-        public static final int oben=0x7f05000a;
-        public static final int roadmap=0x7f050009;
-        public static final int scrollview=0x7f050008;
-        public static final int textView1=0x7f050002;
-        public static final int timeline=0x7f05000b;
-        public static final int unten=0x7f05000d;
+        public static final int Pawn=0x7f050004;
+        public static final int beschriftung=0x7f05000e;
+        public static final int buttons=0x7f050005;
+        public static final int createItemDialog=0x7f050000;
+        public static final int editText1=0x7f050002;
+        public static final int item=0x7f050003;
+        public static final int linearLayout1=0x7f050007;
+        public static final int loadRoadmap=0x7f050009;
+        public static final int newRoadmap=0x7f050008;
+        public static final int newRoadmapName=0x7f050006;
+        public static final int oben=0x7f05000c;
+        public static final int roadmap=0x7f05000b;
+        public static final int scrollview=0x7f05000a;
+        public static final int textView1=0x7f050001;
+        public static final int timeline=0x7f05000d;
+        public static final int unten=0x7f05000f;
     }
     public static final class layout {
-        public static final int item=0x7f030000;
-        public static final int item_unten=0x7f030001;
-        public static final int main=0x7f030002;
-        public static final int simpl_list_item=0x7f030003;
-        public static final int timeline=0x7f030004;
+        public static final int createitem=0x7f030000;
+        public static final int item=0x7f030001;
+        public static final int item_unten=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int simpl_list_item=0x7f030004;
+        public static final int timeline=0x7f030005;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
