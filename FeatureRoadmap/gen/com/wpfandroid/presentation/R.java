7bc3ef644b716ce688d4b077f7d17714596253b1
koto1011 <koto1011@hs-karlsruhe.de>
2011-12-18 22:09:22 +0100

    Layouts für andere Orientation hinzugefügt (weitestgehend identisch)

--- FeatureRoadmap/gen/com/wpfandroid/presentation/R.java
+++ FeatureRoadmap/gen/com/wpfandroid/presentation/R.java
@@ -44,12 +44,13 @@ public final class R {
     }
     public static final class layout {
         public static final int createitem=0x7f030000;
-        public static final int createroadmap=0x7f030001;
-        public static final int item=0x7f030002;
-        public static final int item_unten=0x7f030003;
-        public static final int main=0x7f030004;
-        public static final int simpl_list_item=0x7f030005;
-        public static final int timeline=0x7f030006;
+        public static final int createmilestone=0x7f030001;
+        public static final int createroadmap=0x7f030002;
+        public static final int item=0x7f030003;
+        public static final int item_unten=0x7f030004;
+        public static final int main=0x7f030005;
+        public static final int simpl_list_item=0x7f030006;
+        public static final int timeline=0x7f030007;
     }
     public static final class string {
         public static final int Cancel=0x7f040005;
