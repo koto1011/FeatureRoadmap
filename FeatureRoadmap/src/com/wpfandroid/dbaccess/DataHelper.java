9cae382aeb2d52273d685e33f02bde97f5af99cc
Tom Seidel <seto1011@hs-karlsruhe.de>
2011-12-20 20:25:49 +0100

    Siehe Mail

--- FeatureRoadmap/src/com/wpfandroid/dbaccess/DataHelper.java
+++ FeatureRoadmap/src/com/wpfandroid/dbaccess/DataHelper.java
@@ -27,7 +27,7 @@ public class DataHelper {
 	private SQLiteStatement insertStmtRoadmap;
 	private SQLiteStatement insertStmtMilestone;
 
-	private SQLiteStatement selectStmtRoadmap;
+	private SQLiteStatement updateStmtMilestoneById;
 
 	private static final String INSERT_ROADMAP = "insert into "
 			+ TABLE_NAME_ROADMAP
@@ -36,21 +36,97 @@ public class DataHelper {
 			+ TABLE_NAME_MILESTONE
 			+ "(name, description, date, roadmap_id) values (?, ?, ?, ?)";
 
-	private static final String SELECT_ROADMAP = "SELECT * FROM "
-			+ TABLE_NAME_ROADMAP + " WHERE (id) = (?)";
+	private static final String UPDATE_MILESTONE_BY_ID = "UPDATE "
+			+ TABLE_NAME_MILESTONE
+			+ " SET (name) = (?), (description) = (?), (date) = (?)"
+			+ " WHERE (id) = (?)'";
 
 	public DataHelper(Context context) {
 		this.context = context;
 		OpenHelper openHelper = new OpenHelper(this.context);
+		this.context.deleteDatabase(DATABASE_NAME);
+		this.context.deleteDatabase("example.db");
 		this.db = openHelper.getWritableDatabase();
-//		this.context.deleteDatabase(DATABASE_NAME);
+		// openHelper.onUpgrade(db, 1, 2);
 		this.insertStmtRoadmap = this.db.compileStatement(INSERT_ROADMAP);
 		this.insertStmtMilestone = this.db.compileStatement(INSERT_MILESTONE);
-		this.selectStmtRoadmap = this.db.compileStatement(SELECT_ROADMAP);
+		this.updateStmtMilestoneById = this.db
+				.compileStatement(UPDATE_MILESTONE_BY_ID);
 	}
 
-	public void selectRoadmap(int id) {
-		this.selectStmtRoadmap.bindLong(1, id);
+	public Roadmap getRoadmapByName(String name) {
+		Log.d("EXAMPLE", "getRoadmapByName - Begin");
+		Roadmap roadmap = null;
+		Cursor cursor = this.db.query(TABLE_NAME_ROADMAP, new String[] {
+				"id", "name", "start_date", "end_date", "project_id" }, "name = "
+				+ name, null, null, null, null);
+
+		if (!cursor.moveToFirst())
+			Log.d("EXAMPLE", "cursor is empty");
+
+		if (cursor.moveToFirst()) {
+			roadmap = new Roadmap(cursor.getInt(0),
+					cursor.getString(1), cursor.getString(2),
+					cursor.getString(3), cursor.getInt(4));
+		}
+
+		if (cursor != null && !cursor.isClosed()) {
+			cursor.close();
+			Log.d("EXAMPLE", "cursor is closed");
+		}
+
+		Log.d("EXAMPLE", "getRoadmapByName - End");
+		return roadmap;
+	}
+
+	public Roadmap getRoadmapById(int id) {
+		Log.d("EXAMPLE", "getRoadmapById - Begin");
+		Roadmap roadmap = null;
+		Cursor cursor = this.db.query(TABLE_NAME_ROADMAP, new String[] {
+				"id", "name", "start_date", "end_date", "project_id" }, "id = "
+				+ String.valueOf(id), null, null, null, null);
+
+		if (!cursor.moveToFirst())
+			Log.d("EXAMPLE", "cursor is empty");
+
+		if (cursor.moveToFirst()) {
+			roadmap = new Roadmap(cursor.getInt(0),
+					cursor.getString(1), cursor.getString(2),
+					cursor.getString(3), cursor.getInt(4));
+		}
+
+		if (cursor != null && !cursor.isClosed()) {
+			cursor.close();
+			Log.d("EXAMPLE", "cursor is closed");
+		}
+
+		Log.d("EXAMPLE", "getRoadmapById - End");
+		return roadmap;
+	}
+	
+	public Milestone getMilestoneById(int id) {
+		Log.d("EXAMPLE", "getRoadmapById - Begin");
+		Milestone milestone = null;
+		Cursor cursor = this.db.query(TABLE_NAME_MILESTONE, new String[] {
+				"id", "name", "discription", "date", "roadmap_id" }, "id = "
+				+ String.valueOf(id), null, null, null, null);
+
+		if (!cursor.moveToFirst())
+			Log.d("EXAMPLE", "cursor is empty");
+
+		if (cursor.moveToFirst()) {
+			milestone = new Milestone(cursor.getInt(0),
+					cursor.getString(1), cursor.getString(2),
+					cursor.getString(3), this.getRoadmapById(cursor.getInt(4)));
+		}
+
+		if (cursor != null && !cursor.isClosed()) {
+			cursor.close();
+			Log.d("EXAMPLE", "cursor is closed");
+		}
+
+		Log.d("EXAMPLE", "getRoadmapById - End");
+		return milestone;
 	}
 
 	public long createRoadmap(String name, String start_date, String end_date,
@@ -71,6 +147,14 @@ public class DataHelper {
 		return this.insertStmtMilestone.executeInsert();
 	}
 
+	public long updateMilestone(Milestone milestone) {
+		this.updateStmtMilestoneById.bindString(1, milestone.getName());
+		this.updateStmtMilestoneById.bindString(2, milestone.getDescription());
+		this.updateStmtMilestoneById.bindString(3, milestone.getDate());
+		this.updateStmtMilestoneById.bindLong(4, milestone.getId());
+		return this.updateStmtMilestoneById.executeInsert();
+	}
+
 	public void deleteAllRoadmaps() {
 		this.db.delete(TABLE_NAME_ROADMAP, null, null);
 	}
@@ -79,7 +163,7 @@ public class DataHelper {
 		this.db.delete(TABLE_NAME_MILESTONE, null, null);
 	}
 
-	public List<String> selectAllRoadmapNames() {
+	public List<String> getAllRoadmapNames() {
 		List<String> list = new ArrayList<String>();
 		Cursor cursor = this.db.query(TABLE_NAME_ROADMAP,
 				new String[] { "name" }, null, null, null, null, "name desc");
@@ -94,7 +178,7 @@ public class DataHelper {
 		return list;
 	}
 
-	public List<String> selectAllMilestoneNames() {
+	public List<String> getAllMilestoneNames() {
 		List<String> list = new ArrayList<String>();
 		Cursor cursor = this.db.query(TABLE_NAME_MILESTONE,
 				new String[] { "name" }, null, null, null, null, "name desc");
@@ -109,21 +193,22 @@ public class DataHelper {
 		return list;
 	}
 
-	public List<Milestone> selectAllMilestones() {
-		Log.d("EXAMPLE", "selectAllMilestones - Begin");
+	public List<Milestone> getAllMilestones() {
+		Log.d("EXAMPLE", "getAllMilestones - Begin");
 		List<Milestone> milestones = new ArrayList<Milestone>();
 		Cursor cursor = this.db.query(TABLE_NAME_MILESTONE, new String[] {
 				"id", "name", "description", "date", "roadmap_id" }, null,
 				null, null, null, "id desc");
-		
-		if (!cursor.moveToFirst()) Log.d("EXAMPLE", "cursor is empty");
-	  
+
+		if (!cursor.moveToFirst())
+			Log.d("EXAMPLE", "cursor is empty");
+
 		if (cursor.moveToFirst()) {
 			do {
 				Log.d("EXAMPLE", "cursordurchlauf - begin");
 				Milestone milestone = new Milestone(cursor.getInt(0),
 						cursor.getString(1), cursor.getString(2),
-						cursor.getString(3), null);
+						cursor.getString(3), this.getRoadmapById(cursor.getInt(4)));
 				milestones.add(milestone);
 				Log.d("EXAMPLE", "cursordurchlaufende");
 			} while (cursor.moveToNext());
@@ -132,24 +217,25 @@ public class DataHelper {
 			cursor.close();
 			Log.d("EXAMPLE", "cursor is closed");
 		}
-		
-		Log.d("EXAMPLE", "selectAllMilestones - End");
+
+		Log.d("EXAMPLE", "getAllMilestones - End");
 		return milestones;
 	}
-	
-	public List<Roadmap> selectAllRoadmaps() {
-		Log.d("EXAMPLE", "selectAllRoadmaps - Begin");
+
+	public List<Roadmap> getAllRoadmaps() {
+		Log.d("EXAMPLE", "getAllRoadmaps - Begin");
 		List<Roadmap> roadmaps = new ArrayList<Roadmap>();
-		Cursor cursor = this.db.query(TABLE_NAME_ROADMAP, new String[] {
-				"id", "name", "start_date", "end_date", "project_id" }, null,
-				null, null, null, "id asc");
-		
-		if (!cursor.moveToFirst()) Log.d("EXAMPLE", "cursor is empty");
-	  
+		Cursor cursor = this.db.query(TABLE_NAME_ROADMAP, new String[] { "id",
+				"name", "start_date", "end_date", "project_id" }, null, null,
+				null, null, "id asc");
+
+		if (!cursor.moveToFirst())
+			Log.d("EXAMPLE", "cursor is empty");
+
 		if (cursor.moveToFirst()) {
 			do {
 				Log.d("EXAMPLE", "cursordurchlauf - begin");
-				Roadmap roadmap = new Roadmap (cursor.getInt(0),
+				Roadmap roadmap = new Roadmap(cursor.getInt(0),
 						cursor.getString(1), cursor.getString(2),
 						cursor.getString(3), cursor.getInt(4));
 				roadmaps.add(roadmap);
@@ -160,20 +246,22 @@ public class DataHelper {
 			cursor.close();
 			Log.d("EXAMPLE", "cursor is closed");
 		}
-		
-		Log.d("EXAMPLE", "selectAllRoadmaps - End");
+
+		Log.d("EXAMPLE", "getAllRoadmaps - End");
 		return roadmaps;
 	}
-	
+
 	public List<Milestone> getAllMilestonesByRoadmapId(int roadmapId) {
 		Log.d("EXAMPLE", "getAllMilestonesByRoadmapId - Begin");
 		List<Milestone> milestones = new ArrayList<Milestone>();
 		Cursor cursor = this.db.query(TABLE_NAME_MILESTONE, new String[] {
-				"id", "name", "description", "date", "roadmap_id" }, "roadmap_id = " + String.valueOf(roadmapId),
-				null, null, null, "id desc");
-		
-		if (!cursor.moveToFirst()) Log.d("EXAMPLE", "cursor is empty");
-	  
+				"id", "name", "description", "date", "roadmap_id" },
+				"roadmap_id = " + String.valueOf(roadmapId), null, null, null,
+				"id desc");
+
+		if (!cursor.moveToFirst())
+			Log.d("EXAMPLE", "cursor is empty");
+
 		if (cursor.moveToFirst()) {
 			do {
 				Log.d("EXAMPLE", "cursordurchlauf - begin");
@@ -188,9 +276,9 @@ public class DataHelper {
 			cursor.close();
 			Log.d("EXAMPLE", "cursor is closed");
 		}
-		
+
 		Log.d("EXAMPLE", "getAllMilestonesByRoadmapId - End");
-		return milestones;		
+		return milestones;
 	}
 
 	public void closeDB() {
@@ -207,10 +295,10 @@ public class DataHelper {
 		public void onCreate(SQLiteDatabase db) {
 			db.execSQL("CREATE TABLE "
 					+ TABLE_NAME_ROADMAP
-					+ "(id INTEGER PRIMARY KEY, name TEXT, start_date TEXT, end_date TEXT, project_id INTEGER)");
+					+ "(id INTEGER PRIMARY KEY, name TEXT UNIQUE, start_date TEXT, end_date TEXT, project_id INTEGER)");
 			db.execSQL("CREATE TABLE "
 					+ TABLE_NAME_MILESTONE
-					+ "(id INTEGER PRIMARY KEY, name TEXT, description TEXT, date TEXT, roadmap_id INTEGER)");
+					+ "(id INTEGER PRIMARY KEY, name TEXT UNIQUE, description TEXT, date TEXT, roadmap_id INTEGER)");
 		}
 
 		@Override
